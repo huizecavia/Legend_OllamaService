29f7aead8b19bb7816cabc9355265dc23b39c19e
Matthijs <bomaltennishal@outlook.com>
2026-01-21 18:45:35 +0100

    nieuw

---
+++ src/main/java/io/matthijs/service/OllamaService/OllamaServiceApplication.java
@@ -0,0 +1,13 @@
+package io.matthijs.service.OllamaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/matthijs/service/OllamaService/OllamaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.matthijs.service.OllamaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
